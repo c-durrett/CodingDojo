798a57d0693f9f40d7e9795c5debbfa1d9f17030
c-durrett <durrett.xtian@gmail.com>
2021-07-19 19:47:42 -0400

    initial commit

---
+++ Durrett_Christian_JS_Tinkering.js
@@ -0,0 +1,11 @@
+var childHeight = 48
+
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!")
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.")
+    } 
+}
+console.log(displayIfChildIsAbleToRideTheRollerCoaster(childHeight))
--- Example/README.md
+++ Example/README.md
@@ -1,4 +1,4 @@
 # Hello World
 
-
+Hello
 This is a readme for our first project!
