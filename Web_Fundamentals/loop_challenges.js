5e17439b277743bfe69cd9bff1c0c6488b1a3cc5
c-durrett <durrett.xtian@gmail.com>
2021-07-26 19:28:30 -0400

    +loop_challenges.js (3/5 done)

---
+++ Web_Fundamentals/loop_challenges.js
@@ -0,0 +1,20 @@
+// Challenge 1. Prints odds 1-20
+// for(var i=1; i<=20; i++) {
+//     if(i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// Challenge 2. Decreasing Multiples of 3 from 100-0
+// for(var i=100; i>=0; i--) {
+//     if(i % 3 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// Challenge 3. Print the Sequence "4, 2.5, 1, -0.5, -2, -3.5"
+// arr=[4, 2.5, 1, -0.5, -2, -3.5]
+// for(var i=0; i<arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
