7229251b9f1d5544756d1646672462843044b217
c-durrett <durrett.xtian@gmail.com>
2021-08-03 17:18:57 -0400

    updated user_practice.py

--- python/fundamentals/fundamentals/user_practice.py
+++ python/fundamentals/fundamentals/user_practice.py
@@ -8,39 +8,31 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print(f"User:{self.name}, Balance: ${self.account_balance}")
+        return self
     
     def transfer_money(self, amount, user):
         self.account_balance -= amount
         user.account_balance += amount
         self.display_user_balance()
         user.display_user_balance()
+        return self
 
-guido = User("Guido van Rossum", "guido@python.com")
+sean = User("Sean van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
-marty = User("Marty Fly", "marty@python.com")
+bryce = User("Bryce Fly", "marty@python.com")
 
-guido.make_deposit(100)
-guido.make_deposit(200)
-guido.make_deposit(300)
-guido.make_withdrawal(500)
-guido.display_user_balance()
+sean.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(500).display_user_balance()
 
-monty.make_deposit(200)
-monty.make_deposit(300)
-monty.make_withdrawal(50)
-monty.make_withdrawal(50)
-monty.display_user_balance()
+monty.make_deposit(200).make_deposit(300).make_withdrawal(50).make_withdrawal(50).display_user_balance()
 
-marty.make_deposit(1000)
-marty.make_withdrawal(200)
-marty.make_withdrawal(200)
-marty.make_withdrawal(600)
-marty.display_user_balance()
+bryce.make_deposit(1000).make_withdrawal(200).make_withdrawal(200).make_withdrawal(600).display_user_balance()
 
-guido.transfer_money(100, marty)
+sean.transfer_money(100, bryce)
