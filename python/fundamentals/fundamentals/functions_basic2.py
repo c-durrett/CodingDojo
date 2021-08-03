ebcd409def5ab2358f804f96d4e670f818f1dc9e
c-durrett <durrett.xtian@gmail.com>
2021-08-02 21:01:05 -0400

    finished functions_basics2.py

--- python/fundamentals/fundamentals/functions_basic2.py
+++ python/fundamentals/fundamentals/functions_basic2.py
@@ -20,3 +20,25 @@ def first_plus_len(list):
 print(first_plus_len([1,2,3,4,5]))
 
 # 4. Values Greater than Second
+def vals_greaterthan_second(list):
+    if len(list) < 2:
+        return False
+    output = []
+    for num in range(0, len(list)):
+        if list[num] > list[1]:
+            output.append(list[num])
+    print(len(output))
+    return output
+print(vals_greaterthan_second([5,2,3,2,1,4]))
+print(vals_greaterthan_second([3]))
+
+# 5. This Length, That Value - 
+# Write a function that accepts two integers as parameters: size and value. 
+# The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
+def len_that_val(size, value):
+    list = []
+    for num in range(0, size):
+        list.append(value)
+    return list
+print(len_that_val(4,7))
+print(len_that_val(6,2))
