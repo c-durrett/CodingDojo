3c375e650d43b13b815562852f8ce4a588cc39d1
c-durrett <durrett.xtian@gmail.com>
2021-08-03 19:21:43 -0400

    updated bank_account with @classmethod

--- python/fundamentals/fundamentals/bank_account.py
+++ python/fundamentals/fundamentals/bank_account.py
@@ -1,9 +1,11 @@
 class BankAccount:
     # int_rate = 0.01
     # balance = 0
+    accounts = []
     def __init__(self, int_rate, balance):
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
@@ -25,6 +27,11 @@ class BankAccount:
         if self.balance > 0:
             self.balance += (self.balance * self.int_rate)
         return self
+
+    @classmethod
+    def all_bank_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
     
 account1 = BankAccount(0.02 ,500)
 account2 = BankAccount(0.01 ,0)
@@ -32,6 +39,4 @@ account2 = BankAccount(0.01 ,0)
 account1.deposit(100).deposit(100).deposit(100).withdrawal(150).yield_interest().display_account_info()
 account2.deposit(10).deposit(20).withdrawal(10).withdrawal(10).withdrawal(5).withdrawal(6).yield_interest().display_account_info()
 
-# @classmethod
-# def all_bank_info(cls):
-#     for 
+BankAccount.all_bank_accounts()
