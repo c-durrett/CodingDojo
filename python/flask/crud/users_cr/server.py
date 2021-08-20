999b481fbc3053929b5fd4fa86927e7b6cf9dce4
c-durrett <durrett.xtian@gmail.com>
2021-08-19 22:19:03 -0400

    updated users_cr proj.

--- python/flask/crud/users_cr/server.py
+++ python/flask/crud/users_cr/server.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from users import User
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
+    return redirect('/users')
+
+@app.route('/users')
+def users():
     users = User.get_all()
     return render_template("users.html", users=users)
 
-@app.route('/new')
+@app.route('/user/new')
+def new():
+    return render_template("users_new.html")
+
+@app.route('/user/create', methods=['POST'])
 def add_user():
-# next step is adding the form to create new users
+    data = {
+        'first_name':request.form['first_name'],
+        'last_name':request.form['last_name'],
+        'email':request.form['email']
+    }
+    print(request.form)
+    User.save(data)
+    return redirect('/users')
+
 
 if __name__=="__main__":
     app.run(debug=True)
--- python/flask/crud/users_cr/users.py
+++ python/flask/crud/users_cr/users.py
@@ -18,3 +18,10 @@ class User:
             users.append(cls(user))
         return users
         
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s,%(last_name)s,%(email)s);"
+
+        # comes back as the new row id
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return result
