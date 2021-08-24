b0013337db8ecedbbf40751c4d6864f7f25d6dc0
c-durrett <durrett.xtian@gmail.com>
2021-08-24 16:11:37 -0400

    + login_and_reg project (finished)

---
+++ python/flask/flask_mysql/login_and_registration/flask_app/controllers/users.py
@@ -0,0 +1,69 @@
+from flask_app import app
+
+from flask import render_template, redirect, request, session, flash
+
+from flask_bcrypt import Bcrypt
+
+from flask_app.models.user import User
+
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+    # validate user
+    if (User.validate_registration(request.form)) == True:
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])
+        data = {
+            'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'email': request.form['email'],
+            'password': pw_hash
+        }
+        User.create_user(data)
+        flash("You've registered, please log in!")
+    # if user is valid, create record in DB
+    # redirect back to index
+    return redirect('/')
+
+@app.route('/users/login', methods=['POST'])
+def login_user():
+
+    # check if user exists
+    users = User.get_user_by_email(request.form)
+    if len(users) != 1:
+        flash('Email is incorerct.')
+        return redirect('/')
+
+    user = users[0]
+
+    # if user exists, check password
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash('Password is incorrect.')
+        return redirect('/')
+
+    # save data in session and redirect to the success page
+    session['user_id'] = user.id
+    session['user_first_name'] = user.first_name
+    session['user_last_name'] = user.last_name
+    session['user_email'] = user.email
+    return redirect('/success')
+# normally we would redirect to next page
+
+@app.route('/success')
+def success():
+    if 'user_id' not in session:
+        flash('Please log in to view this page.')
+        return redirect('/')
+
+    return render_template('success.html')
+
+@app.route('/users/logout')
+def logout():
+    session.clear()
+    flash("You've logged out successfully.")
+    return redirect('/')
+
---
+++ python/flask/flask_mysql/login_and_registration/flask_app/models/user.py
@@ -0,0 +1,72 @@
+import re
+
+from flask import flash
+from flask_app import app
+from flask_app.config.mysqlconnection import MySQLConnection
+
+class User():
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def create_user(cls, data):
+        query = 'INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);'
+
+        result = MySQLConnection('login_and_reg_schema').query_db(query, data)
+
+        return result
+
+    @staticmethod
+    def validate_registration(data):
+        is_valid = True
+
+        email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+        if len(data['first_name']) < 2 or len(data['first_name']) > 32:
+            is_valid = False
+            flash('First name should be between 2 and 32 characters.')
+
+        if len(data['last_name']) < 2 or len(data['last_name']) > 32:
+            is_valid = False
+            flash('Last name should be between 2 and 32 characters.')
+
+        if not email_regex.match(data['email']):
+            is_valid = False
+            flash('Email is not in correct format')
+
+        if len(data['email']) < 7 or len(data['email']) > 255:
+            is_valid = False
+            flash('Email must be between 7 and 255 characters.')
+
+        if len(User.get_user_by_email(data)) > 0:
+            is_valid = False
+            flash('Email address is already in use.')
+
+        if len(data['password']) < 8:
+            is_valid = False
+            flash('Password should be at least 8 characters in length.')
+
+        if data['password'] != data['confirm_password']:
+            is_valid = False
+            flash('Password & Confirm Password do not match.')
+
+        return is_valid
+
+    @classmethod
+    def get_user_by_email(cls, data):
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+
+        results = MySQLConnection('login_and_reg_schema').query_db(query, data)
+        
+        users = []
+
+        for line in results:
+            users.append(User(line))
+
+        return users
